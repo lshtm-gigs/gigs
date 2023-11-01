bd4836e10b78a6e28649035e56d636c5b590f4d7
Simon Parker <65285181+awadafuk@users.noreply.github.com>
2023-11-01 17:30:50 +0000

    BUGFIX: Changing `sapply` to `vapply` and some changes to make lintr happy.

--- data-raw/ig_nbs.R
+++ data-raw/ig_nbs.R
@@ -1,20 +1,26 @@
 make_ig_nbs_list <- function(raw_stem, x, y) {
   data_dir <- file.path("data-raw", "tables", "ig_nbs")
   make_zscore_tbl <- function(sex) {
-    tbl <- read.csv(file = file.path(data_dir, paste0(raw_stem, sex, "_zscores.csv")), fileEncoding = "UTF-8-BOM",
+    tbl <- read.csv(file = file.path(data_dir, paste0(raw_stem, sex,
+                                                      "_zscores.csv")),
+                    fileEncoding = "UTF-8-BOM",
                     header = F, sep = " ") |>
       as.data.frame() |>
-      dplyr::rename(gest_age = V1, SD3neg = V2, SD2neg = V3, SD1neg = V4, SD0 = V5, SD1 = V6, SD2 = V7, SD3 = V8) |>
+      dplyr::rename(gest_age = V1, SD3neg = V2, SD2neg = V3, SD1neg = V4,
+                    SD0 = V5, SD1 = V6, SD2 = V7, SD3 = V8) |>
       dplyr::mutate(gest_age = 168:300)
     return(tbl)
   }
 
   make_percentile_tbl <- function(sex) {
-    tbl <- read.csv(file = file.path(data_dir, paste0(raw_stem, sex, "_percentiles.csv")), fileEncoding = "UTF-8-BOM",
-                         header = F,
+    tbl <- read.csv(file = file.path(data_dir,
+                                     paste0(raw_stem, sex, "_percentiles.csv")),
+                    fileEncoding = "UTF-8-BOM",
+                    header = F,
                     sep = " ") |>
       as.data.frame() |>
-      dplyr::rename(gest_age = V1, P03 = V2, P05 = V3, P10 = V4, P50 = V5, P90 = V6, P95 = V7, P97 = V8) |>
+      dplyr::rename(gest_age = V1, P03 = V2, P05 = V3, P10 = V4, P50 = V5,
+                    P90 = V6, P95 = V7, P97 = V8) |>
       dplyr::mutate(gest_age = 168:300)
     return(tbl)
   }
@@ -27,20 +33,31 @@ make_ig_nbs_list <- function(raw_stem, x, y) {
 }
 
 
-ig_nbs_wfga <- make_ig_nbs_list(raw_stem = "ig_nbs_weight_", x = "ga_weeks", y = "weight_kg")
-ig_nbs_lfga <- make_ig_nbs_list(raw_stem = "ig_nbs_len_", x = "ga_weeks", y = "len_cm")
-ig_nbs_hcfga <- make_ig_nbs_list(raw_stem = "ig_nbs_hc_", x = "ga_weeks", y = "hc_cm")
-ig_nbs_wlrfga <- make_ig_nbs_list(raw_stem = "ig_nbs_weightlenratio_", x = "ga_weeks", y = "weight_len_ratio")
+ig_nbs_wfga <- make_ig_nbs_list(raw_stem = "ig_nbs_weight_",
+                                x = "ga_weeks",
+                                y = "weight_kg")
+ig_nbs_lfga <- make_ig_nbs_list(raw_stem = "ig_nbs_len_",
+                                x = "ga_weeks",
+                                y = "len_cm")
+ig_nbs_hcfga <- make_ig_nbs_list(raw_stem = "ig_nbs_hc_",
+                                 x = "ga_weeks",
+                                 y = "hc_cm")
+ig_nbs_wlrfga <- make_ig_nbs_list(raw_stem = "ig_nbs_weightlenratio_",
+                                  x = "ga_weeks",
+                                  y = "weight_len_ratio")
 
 
 make_ig_nbs_bodycomp_list <- function(raw_stem, x, y) {
   data_dir <- file.path("data-raw", "tables", "ig_nbs")
   make_percentile_tbl <- function(sex) {
-    tbl <- read.csv(file = file.path(data_dir, paste0(raw_stem, sex, "_percentiles.csv")), fileEncoding = "UTF-8-BOM",
+    tbl <- read.csv(file = file.path(data_dir,
+                                     paste0(raw_stem, sex, "_percentiles.csv")),
+                    fileEncoding = "UTF-8-BOM",
                          header = F,
                     sep = ",") |>
       as.data.frame() |>
-      dplyr::rename(gest_age = V1, P03 = V2, P10 = V3, P50 = V4, P90 = V5, P97 = V6)
+      dplyr::rename(gest_age = V1, P03 = V2, P10 = V3, P50 = V4, P90 = V5,
+                    P97 = V6)
    return(tbl)
   }
   return(list(male = list(percentiles = make_percentile_tbl(sex = "male")),
@@ -48,9 +65,15 @@ make_ig_nbs_bodycomp_list <- function(raw_stem, x, y) {
               x = x,
               y = y))
 }
-ig_nbs_ffmfga <- make_ig_nbs_bodycomp_list(raw_stem = "ig_nbs_ffmfga_", x = "ga_weeks", y = "fatfree_mass_g")
-ig_nbs_bfpfga <- make_ig_nbs_bodycomp_list(raw_stem = "ig_nbs_bfpfga_", x = "ga_weeks", y = "bodyfat_percentage")
-ig_nbs_fmfga <- make_ig_nbs_bodycomp_list(raw_stem = "ig_nbs_fmfga_", x = "ga_weeks", y = "fatmass_g")
+ig_nbs_ffmfga <- make_ig_nbs_bodycomp_list(raw_stem = "ig_nbs_ffmfga_",
+                                           x = "ga_weeks",
+                                           y = "fatfree_mass_g")
+ig_nbs_bfpfga <- make_ig_nbs_bodycomp_list(raw_stem = "ig_nbs_bfpfga_",
+                                           x = "ga_weeks",
+                                           y = "bodyfat_percentage")
+ig_nbs_fmfga <- make_ig_nbs_bodycomp_list(raw_stem = "ig_nbs_fmfga_",
+                                          x = "ga_weeks",
+                                          y = "fatmass_g")
 
 ig_nbs <- list(
   wfga = ig_nbs_wfga,
@@ -78,9 +101,12 @@ ig_coeffs_readxl_wrap <- function(path) {
 
 # Using tables provided by Eric Ohuma
 ig_nbs_coeffs <- list(
-  wfga = ig_coeffs_readxl_wrap(path = "data-raw/tables/ig_nbs/Newborn standards parameters (BW).xlsx"),
-  lfga = ig_coeffs_readxl_wrap(path = "data-raw/tables/ig_nbs/Newborn standards parameters (BL).xlsx"),
-  hcfga = ig_coeffs_readxl_wrap(path = "data-raw/tables/ig_nbs/Newborn standards parameters (HC).xlsx")
+  wfga = ig_coeffs_readxl_wrap(
+    path = "data-raw/tables/ig_nbs/Newborn standards parameters (BW).xlsx"),
+  lfga = ig_coeffs_readxl_wrap(
+    path = "data-raw/tables/ig_nbs/Newborn standards parameters (BL).xlsx"),
+  hcfga = ig_coeffs_readxl_wrap(
+    path = "data-raw/tables/ig_nbs/Newborn standards parameters (HC).xlsx")
 )
 usethis::use_data(ig_nbs_coeffs, overwrite = TRUE)
 
@@ -93,7 +119,6 @@ write_dta_gamlssfiles <- function(gamlss_tbls) {
     .f = ~ {
       tbls <- .x
       acronym <- .y
-      sexes <- names(tbls)
       out <- purrr::map2_dfr(
         .x = tbls, 
         .y = names(tbls), 
@@ -101,7 +126,8 @@ write_dta_gamlssfiles <- function(gamlss_tbls) {
           tbl <- .x
           sex <- .y
           sex_col <- ifelse(sex == "male", 1, 0)
-          gamlss_tbl <- dplyr::mutate(tbl, nbsMSNT_sex = as.integer(sex_col)) |>
+          tbl |>
+            dplyr::mutate(nbsMSNT_sex = as.integer(sex_col)) |>
             dplyr::rename(nbsMSNT_gest_age = gest_age,
                           nbsMSNT_mu = mu,
                           nbsMSNT_sigma = sigma,
--- inst/logo/logo.R
+++ inst/logo/logo.R
@@ -2,16 +2,36 @@
 load("data/ig_nbs.rda")
 long_tbl <- ig_nbs$wfga$female$zscores |>
   dplyr::filter(gest_age > 231)
-tidy_tbl <- tidyr::pivot_longer(long_tbl, cols = tidyselect::starts_with("SD"), names_to = "SD",
+tidy_tbl <- tidyr::pivot_longer(long_tbl,
+                                cols = tidyselect::starts_with("SD"),
+                                names_to = "SD",
                                 values_to = "weight_kg")
 sm <- function(x) predict(smooth.spline(x = x))$y
 curve <- ggplot2::ggplot() +
-  ggplot2::geom_ribbon(data = long_tbl, mapping = ggplot2::aes(x = gest_age, ymax = sm(SD3),    ymin = sm(SD2)), alpha = 0.6, fill = "#ffdab9") +
-  ggplot2::geom_ribbon(data = long_tbl, mapping = ggplot2::aes(x = gest_age, ymax = sm(SD2),    ymin = sm(SD1)), alpha = 0.6, fill = "#ff8c00") +
-  ggplot2::geom_ribbon(data = long_tbl, mapping = ggplot2::aes(x = gest_age, ymax = sm(SD1),    ymin = sm(SD0)), alpha = 0.8, fill = "#fe5a1d") +
-  ggplot2::geom_ribbon(data = long_tbl, mapping = ggplot2::aes(x = gest_age, ymax = sm(SD0),    ymin = sm(SD1neg)), alpha = 0.8, fill = "#fe5a1d") +
-  ggplot2::geom_ribbon(data = long_tbl, mapping = ggplot2::aes(x = gest_age, ymax = sm(SD1neg), ymin = sm(SD2neg)), alpha = 0.6, fill = "#ff8c00") +
-  ggplot2::geom_ribbon(data = long_tbl, mapping = ggplot2::aes(x = gest_age, ymax = sm(SD2neg), ymin = sm(SD3neg)), alpha = 0.6, fill = "#ffdab9") +
+  ggplot2::geom_ribbon(data = long_tbl,
+                       mapping = ggplot2::aes(x = gest_age, ymax = sm(SD3),
+                                              ymin = sm(SD2)), alpha = 0.6,
+                       fill = "#ffdab9") +
+  ggplot2::geom_ribbon(data = long_tbl,
+                       mapping = ggplot2::aes(x = gest_age, ymax = sm(SD2),
+                                              ymin = sm(SD1)), alpha = 0.6,
+                       fill = "#ff8c00") +
+  ggplot2::geom_ribbon(data = long_tbl,
+                       mapping = ggplot2::aes(x = gest_age, ymax = sm(SD1),
+                                              ymin = sm(SD0)), alpha = 0.8,
+                       fill = "#fe5a1d") +
+  ggplot2::geom_ribbon(data = long_tbl,
+                       mapping = ggplot2::aes(x = gest_age, ymax = sm(SD0),
+                                              ymin = sm(SD1neg)), alpha = 0.8,
+                       fill = "#fe5a1d") +
+  ggplot2::geom_ribbon(data = long_tbl,
+                       mapping = ggplot2::aes(x = gest_age, ymax = sm(SD1neg),
+                                              ymin = sm(SD2neg)), alpha = 0.6,
+                       fill = "#ff8c00") +
+  ggplot2::geom_ribbon(data = long_tbl,
+                       mapping = ggplot2::aes(x = gest_age, ymax = sm(SD2neg),
+                                              ymin = sm(SD3neg)), alpha = 0.6,
+                       fill = "#ffdab9") +
   ggplot2::geom_smooth(data = dplyr::filter(tidy_tbl, SD == "SD0"),
                        mapping = ggplot2::aes(x = gest_age, y = weight_kg),
                        colour = "#7f4029", weight = 0.5, alpha = 0.5) +
@@ -25,12 +45,21 @@ name_size <- 10.5
 url_size <- 1.15
 sticker <- hexSticker::sticker(
   filename = temp,
+
   subplot = curve,
   s_x = 1, s_y = 1, s_height = 1.09, s_width = 1.9,
-  package = "gigs", p_size = name_size, p_x = 0.85, p_y = 1.425,  p_color = "#000000", p_family = title_font,
+
+  package = "gigs",
+  p_size = name_size, p_x = 0.85, p_y = 1.425,  p_color = "#000000",
+  p_family = title_font,
+
   h_fill = "#FFFFFF", h_color = "#fe7b3b",
-  url = "https://www.github.com/lshtm-gigs/gigs", u_x = 1.05, u_y = 0.085, u_size = url_size, u_angle = 30, u_family = url_font) +
-  cowplot::draw_image(image = "inst/logo/temp2.png", x = 0.625, y = 0.190, width = 1, height = 1, scale = 0.925)
+
+  url = "https://www.github.com/lshtm-gigs/gigs",
+  u_x = 1.05, u_y = 0.085, u_size = url_size, u_angle = 30,
+  u_family = url_font) +
+  cowplot::draw_image(image = "inst/logo/temp2.png", x = 0.625, y = 0.190,
+                      width = 1, height = 1, scale = 0.925)
 unlink(temp)
 ggplot2::ggsave(sticker,
                 filename = "man/figures/logo.png",
